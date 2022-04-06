c7f23874d34918ff1d4ff84a60f7a8ba2a272d18
Berina Karisik <berinakarisik@gmail.com>
2022-04-06 09:19:39 +0200

    upload2

--- ex11/src/index.js
+++ ex11/src/index.js
@@ -1,15 +1,18 @@
 function functionElse(num){
+    var result;
     if(num <= 5){
-        return "5 or smaller";
+        result = "5 or smaller";
     } else {
-        return "Bigger than 5";
+        result = "Bigger than 5";
     }
+    return result;
 }
 
-functionElse(4);
-functionElse(5);
-functionElse(6);
-functionElse(10);
+console.log(functionElse(4));
+console.log(functionElse(5));
+console.log(functionElse(6));
+console.log(functionElse(10));
+
 
 
 module.exports = functionElse;
